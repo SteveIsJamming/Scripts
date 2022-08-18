6134a195d429a02ce233aaf78803ec5191a23ab6
SteveIsJamming <111202929+SteveIsJamming@users.noreply.github.com>
2022-08-17 21:45:31 -0500

    Update Clicker Madness!.lua
    added remotes

--- Clicker Madness!.lua
+++ Clicker Madness!.lua
@@ -1,6 +1,11 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Clicking Madness", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
---Important thing
+--Important things
+local args = {[1] = 1}
+game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.ClickService.Click:FireServer(unpack(args))
+
+local args = {[1] = 1}
+game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.RebirthService.BuyRebirths:FireServer(unpack(args))
 
 
 --Values
